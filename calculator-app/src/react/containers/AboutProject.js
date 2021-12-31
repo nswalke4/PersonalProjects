9727dd9b4f02f959d84e3aeaca5f6f689376d033
Nicholas Walker <nswalke4@asu.edu>
2021-12-30 20:50:44 -0600

    Adjusts how the project information is displayed

--- calculator-app/src/react/containers/AboutProject.js
+++ calculator-app/src/react/containers/AboutProject.js
@@ -1,19 +1,19 @@
+import ProjectInfo from "../components/ProjectInfo";
+import {useState} from "react";
+
 const AboutProject = () => {
+    const [moreInfo, setMoreInfo] = useState(false);
+
+    const swapInfoState = () => {
+        setMoreInfo(!moreInfo);
+    }
+
     return (
         <div className="about-project">
             <h2>Nicholas Walker's</h2>
             <h1>Basic Calculator Application</h1>
-            <p>
-                Thank you for visiting this web application! This project was completed to become a
-                part of my Personal Software Development Portfolio. This is my very first attempt at
-                creating a website (using CSS and HTML), as well as using Javascript and React JS.
-                Overall, this project is very basic. The website is semi-reactive. However, the
-                actual calculator does not resize. In order for the application to work properly,
-                the display should be at least 600 pixels wide. As for the calculator: it only has
-                basic functionality. The main goal of this application was to test out web
-                development and to test my skills in a new language; developing a calculator that
-                can do many complicated things was not a main priority.
-            </p>
+            {moreInfo && <ProjectInfo swapState={swapInfoState} />}
+            {!moreInfo && <h3 onClick={swapInfoState}>About this Project</h3>}
         </div>
     );
 };
