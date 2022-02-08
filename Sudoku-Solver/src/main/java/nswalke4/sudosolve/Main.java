d56690ff18613597d3a25cfff37442890104f360
Nicholas Walker <nswalke4@asu.edu>
2022-02-08 02:25:55 -0600

    Changes the program execution to a shell script instead of gradle

--- Sudoku-Solver/src/main/java/nswalke4/sudosolve/Main.java
+++ Sudoku-Solver/src/main/java/nswalke4/sudosolve/Main.java
@@ -1,5 +1,8 @@
 package nswalke4.sudosolve;
 
-public static void main(String[] args) {
-    System.out.println("Welcome to the Sudoku Solver!");
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Sudoku Solver!");
+    }
 }
