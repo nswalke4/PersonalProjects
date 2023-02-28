594f75721ba64d40e36a6b35c7b0dc2b480cd299
Walker <nicholas.walker@dish.com>
2023-02-27 17:07:33 -0700

    Completes the challenge for section 10

--- udemy/BeginningC++Programming/Section10Challenge/main.cpp
+++ udemy/BeginningC++Programming/Section10Challenge/main.cpp
@@ -7,6 +7,8 @@
  */
 
 #include <iostream>
+#include <cctype>
+#include <string>
 
 using namespace std;
 
@@ -14,7 +16,39 @@ int main(int argc, char *argv[])
 {
     string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     string key {"XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr"};
+    string message {};
 
-    cout << "\nWelcome to the message cypher program!!";
+    cout << "\nWelcome to the message cypher program!!\n";
+    cout << "---------------------------------------\n\n";
+    cout << "Please enter the message you would like encrypted:\n";
+    getline(cin, message);
+
+    cout << "\n\n....... got it, Message Received .......\n";
+
+    // encrypt message
+    cout << "\n...............Encrypting...............";
+    for (size_t i = 0; i < message.size(); i++)
+    {
+        char c {message.at(i)};
+        if (isalpha(c))
+        {
+            int index {static_cast<int>(alphabet.find(c))};
+            message.at(i) = key.at(index);
+        }
+    }
+    cout << "\n\nEncrypted Message:\n" << message;
+
+    // decrypt message
+    cout << "\n\n...............Decrypting...............";
+    for (size_t i = 0; i < message.size(); i++)
+    {
+        char c {message.at(i)};
+        if (isalpha(c))
+        {
+            int index {static_cast<int>(key.find(c))};
+            message.at(i) = alphabet.at(index);
+        }
+    }
+    cout << "\n\nDecrypted Message:\n" << message;
     return 0;
 }
