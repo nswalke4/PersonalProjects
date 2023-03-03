55927aab39c19f34554e25b80c2648b3912c6fc0
Nicholas Walker <nswalke4@asu.edu>
2023-03-02 18:42:29 -0700

    Adds the section 14 challenge directory and file outlines

---
+++ udemy/BeginningC++Programming/Section14Challenge/README.md
@@ -0,0 +1,18 @@
+# Practice Usage of Operator Overloads
+
+<table>
+<tr>
+<th>Time to Complete</th>
+<td>tbd</td>
+</tr>
+<tr>
+<th>Date Completed</th>
+<td>03/02/2023</td>
+</tr>
+</table>
+
+<hr />
+
+## Directions
+
+
---
+++ udemy/BeginningC++Programming/Section14Challenge/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Nicholas Walker
+ * @date 03/02/2023
+ *
+ * @brief Runs the Operator Overload practice program
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
