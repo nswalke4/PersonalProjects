5309e3be1a544b36461955a0b0c8822fbe91eace
Walker <nicholas.walker@dish.com>
2023-03-03 12:26:49 -0700

    Adds the non-member operator overloads (and some missing binary operators)

--- udemy/TEMPLATES/class.h
+++ udemy/TEMPLATES/class.h
@@ -1,5 +1,5 @@
 /**
- * @file <class.h>
+ * @file class.h--TEMPLATE
  * @author Nicholas Walker
  * @date xx/xx/xxxx
  *
@@ -19,6 +19,32 @@ class Class
     friend ostream &operator<<(ostream &, const Class &);
     friend istream &operator>>(istream &, Class &);
 
+    // Operator Overloads as "Non-Member" Functions
+    friend Class operator+(const Class &);          // Positive Operator Overload
+    friend Class operator-(const Class &);          // Negation Operator Overload
+    friend Class operator++(const Class &);         // Pre-Increment Operator Overload
+    friend Class operator++(const Class &, int);    // Post-Increment Operator Overload
+    friend Class operator--(const Class &);         // Pre-Decrement Operator Overload
+    friend Class operator--(const Class &, int);    // Post-Decrement Operator Overload
+    friend bool operator!(const Class &);           // Not Operator Overload
+
+    friend Class operator+(const Class &, const Class &);   // Addition Operator Overload
+    friend Class operator+=(const Class &, const Class &);  // Increased-By Operator Overload
+    friend Class operator-(const Class &, const Class &);   // Subtraction Operator Overload
+    friend Class operator-=(const Class &, const Class &);  // Decreased-By Operator Overload
+    friend Class operator*(const Class &, const Class &);   // Multiplication Operator Overload
+    friend Class operator*=(const Class &, const Class &);  // Multiplied-By Operator Overload
+    friend Class operator/(const Class &, const Class &);   // Division Operator Overload
+    friend Class operator/=(const Class &, const Class &);  // Divided-By Operator Overload
+    friend Class operator%(const Class &, const Class &);   // Modulo Operator Overload
+    friend Class operator%=(const Class &, const Class &);  // Modulous-Of Operator Overload
+    friend bool operator==(const Class &, const Class &);   // Equal-To Operator Overload
+    friend bool operator!=(const Class &, const Class &);   // Not-Equal-To Operator Overload
+    friend bool operator>(const Class &, const Class &);    // Greater-Than Operator Overload
+    friend bool operator>=(const Class &, const Class &);   // Greater-Than or Equal-To Operator Overload
+    friend bool operator<(const Class &, const Class &);    // Less-Than Operator Overload
+    friend bool operator<=(const Class &, const Class &);   // Less-Than or Equal-To Operator Overload
+
     // other friend variables/functions
 
 private:
@@ -35,6 +61,7 @@ public:
     Class &operator=(const Class &);    // Copy Assignment Operator Overload
     Class &operator=(Class &&);         // Move Assignment Operator Overload
 
+    Class operator+() const;            // Positive Operator Overload
     Class operator-() const;            // Negation Operator Overload
     Class operator++();                 // Pre-Increment Operator Overload
     Class operator++(int);              // Post-Increment Operator Overload
@@ -43,7 +70,15 @@ public:
     bool operator!() const;             // Not Operator Overload
 
     Class operator+(const Class &) const;   // Addition Operator Overload
+    Class operator+=(const Class &) const;  // Increased-By Operator Overload
     Class operator-(const Class &) const;   // Subtraction Operator Overload
+    Class operator-=(const Class &) const;  // Decreased-By Operator Overload
+    Class operator*(const Class &) const;   // Multiplication Operator Overload
+    Class operator*=(const Class &) const;  // Multiplied-By Operator Overload
+    Class operator/(const Class &) const;   // Division Operator Overload
+    Class operator/=(const Class &) const;  // Divided-By Operator Overload
+    Class operator%(const Class &) const;   // Modulo Operator Overload
+    Class operator%=(const Class &) const;  // Modulous-Of Operator Overload
     bool operator==(const Class &) const;   // Equal-To Operator Overload
     bool operator!=(const Class &) const;   // Not-Equal-To Operator Overload
     bool operator>(const Class &) const;    // Greater-Than Operator Overload
